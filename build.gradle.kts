51288df219ade99e881936f192285ecea548f06e
unjifreddy777 <neolgul994@gmail.com>
2025-04-06 00:11:44 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,8 +53,8 @@ tasks {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     withType<JavaCompile> {
