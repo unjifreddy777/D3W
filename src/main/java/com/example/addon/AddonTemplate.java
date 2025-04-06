12a8f8f1939e1b77b0c8ee9059ff33d2a98bbc38
unjifreddy777 <neolgul994@gmail.com>
2025-04-06 12:54:30 +0900

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,14 +1,10 @@
-package com.example.addon;
+ppackage com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.modules.ModuleExample;
 import com.example.addon.modules.TheMinjaeNettyCrasher;
 import com.example.addon.hud.MinjaeHud;
 
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.commands.Commands;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.categories.Category;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 
@@ -17,24 +13,17 @@ import org.slf4j.helpers.NOPLogger;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = NOPLogger.NOP_LOGGER;
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Minjae Addon");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Minjae's Meteor Addon");
 
-        Modules.get().add(new ModuleExample());
+        // Modules
         Modules.get().add(new TheMinjaeNettyCrasher());
 
-        Commands.add(new CommandExample());
-
+        // HUDs
         Hud.get().register(MinjaeHud.INFO);
-    }
-
-    @Override
-    public void onRegisterCategories() {
-        Modules.registerCategory(CATEGORY);
     }
 
     @Override
