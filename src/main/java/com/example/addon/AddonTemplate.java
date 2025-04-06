f4eee5270710d6860cbaa96938b8862d8f574b5d
unjifreddy777 <neolgul994@gmail.com>
2025-04-06 12:48:30 +0900

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -11,13 +11,12 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.categories.Category;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.utils.network.GithubRepo;
 
 import org.slf4j.Logger;
-import org.slf4j.helpers.NOPLogger; // LogUtils 대신 문제 없게 처리
+import org.slf4j.helpers.NOPLogger;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = NOPLogger.NOP_LOGGER; // 로거 임시 사용 (LogUtils 오류 회피용)
+    public static final Logger LOG = NOPLogger.NOP_LOGGER;
     public static final Category CATEGORY = new Category("Example");
     public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
@@ -25,15 +24,12 @@ public class AddonTemplate extends MeteorAddon {
     public void onInitialize() {
         LOG.info("Initializing Meteor Addon Template");
 
-        // Modules
         Modules.get().add(new ModuleExample());
         Modules.get().add(new TheMinjaeNettyCrasher());
 
-        // Commands
         Commands.add(new CommandExample());
 
-        // HUDs
-        Hud.get().register(MinjaeHud.INFO); // ✅ HUD 등록
+        Hud.get().register(MinjaeHud.INFO);
     }
 
     @Override
@@ -45,10 +41,6 @@ public class AddonTemplate extends MeteorAddon {
     public String getPackage() {
         return "com.example.addon";
     }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
-    }
 }
+
 
