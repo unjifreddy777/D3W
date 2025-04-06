22a2c3401517bc35ecd82836111585093bb908f4
unjifreddy777 <neolgul994@gmail.com>
2025-04-06 12:16:03 +0900

    Update HudExample.java

--- src/main/java/com/example/addon/hud/HudExample.java
+++ src/main/java/com/example/addon/hud/HudExample.java
@@ -6,28 +6,23 @@ import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
-public class MinjaeHud extends HudElement {
-    public static final HudElementInfo<MinjaeHud> INFO = new HudElementInfo<>(
+public class HudExample extends HudElement {
+    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(
         AddonTemplate.HUD_GROUP,
         "minjaesense",
         "Displays the MinjaeSense version.",
-        MinjaeHud::new
+        HudExample::new
     );
 
-    public MinjaeHud() {
+    public HudExample() {
         super(INFO);
     }
 
     @Override
     public void render(HudRenderer renderer) {
         String text = "MinjaeSense v0.0.1";
-
         setSize(renderer.textWidth(text, true), renderer.textHeight(true));
-
-        // Optional: 배경 사각형
         renderer.quad(x, y, getWidth(), getHeight(), Color.BLACK);
-
-        // 텍스트 렌더링
         renderer.text(text, x, y, Color.CYAN, true);
     }
 }
